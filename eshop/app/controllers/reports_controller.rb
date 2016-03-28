1b9b917cef38979f77c20ec09d4b8ff5a986e2b8
David Haynes <dhaynesxx@gmail.com>
2016-03-29 10:29:28 +1100

    heroku start

--- eshop/app/controllers/reports_controller.rb
+++ eshop/app/controllers/reports_controller.rb
@@ -2,10 +2,10 @@ class ReportsController < ApplicationController
     before_action :authorise
 
 def inventory
-    products = Product.all
+    @products = Product.all
     @product_inventory_awaiting_delivery =[]
     orders_awaiting_delivery = Order.where(:shipped_date => nil )
-    products.each do |product|
+    @products.each do |product|
         quantity = 0
         orders_awaiting_delivery.each do |order|
             items = LineItem.where(:order_id => order.id).where(:product_id => product.id)
@@ -13,7 +13,6 @@ def inventory
         end
         @product_inventory_awaiting_delivery.push(quantity)
     end
-    @products = Product.all
 end
 
 def sales
--- eshop/db/seeds.rb
+++ eshop/db/seeds.rb
@@ -8,9 +8,13 @@ u3 = User.create :email => 'user1@ga.co', :password => 'chicken', :password_conf
 u3 = User.create :email => 'user2@ga.co', :password => 'chicken', :password_confirmation => 'chicken', :admin => false, :master => false, :name => 'Jane'
 
 Product.destroy_all
-p1 = Product.create :name => "Beer", :image => 'http://www.pizznlove-valdisere.com/wp-content/uploads/2014/02/peroni-nastro-azzuro.png', :inventory => 97, :unit_cost_price => 2.0, :description => "this is a peroni", :price_regular => 5.0
-p2 = Product.create :name => "Wine", :image => 'https://www.wineselectors.com.au/images/sites/1/products/6B7DEAD9-877F-4B7E-8AAC-79737FABAD84/TAYLO30314-bottle.png' , :inventory => 48, :unit_cost_price => 5.0, :description => "this is a merlot", :price_regular => 15.0
+p1 = Product.create :name => "Peroni", :image => 'http://www.pizznlove-valdisere.com/wp-content/uploads/2014/02/peroni-nastro-azzuro.png', :inventory => 97, :unit_cost_price => 2.0, :description => "this is a beer", :price_regular => 5.0
+p2 = Product.create :name => "Merlot", :image => 'https://www.wineselectors.com.au/images/sites/1/products/6B7DEAD9-877F-4B7E-8AAC-79737FABAD84/TAYLO30314-bottle.png' , :inventory => 48, :unit_cost_price => 5.0, :description => "this is a wine", :price_regular => 15.0
 p3 = Product.create :name => "Coke", :image => 'http://pngimg.com/upload/cocacola_PNG21.png', :inventory => 194, :unit_cost_price => 1.0, :description => "this is a soft drink", :price_regular => 2.0, :tax_applies => false
+p4 = Product.create :name => "Corona", :image => 'http://rapilicor.com/wp-content/uploads/2015/08/Corona.png', :inventory => 20, :unit_cost_price => 2.5, :description => "this is a beer", :price_regular => 6.0, :tax_applies => true
+p5 = Product.create :name => "Pinot Noir", :image => 'http://www.luliwines.com/images/luli-pinot-noir-2012.png', :inventory => 10, :unit_cost_price => 5.5, :description => "this is a wine", :price_regular => 18.0, :tax_applies => true
+p6 = Product.create :name => "Sprite", :image => 'http://pngimg.com/upload/sprite_PNG8918.png', :inventory => 1, :unit_cost_price => 1.5, :description => "this is a soft drink", :price_regular => 3.0, :tax_applies => false
+
 
 LineItem.destroy_all
 l1 = LineItem.create :quantity => 3, :unit_price => 5.0, :unit_tax => 0.45, :unit_cost => 2.0
